475b5e2f436f117d6b7a7895c121cdf6ef709475
Juan Camilo Penagos <109445222+Misterdev19@users.noreply.github.com>
2022-08-16 18:29:33 -0500

    Prueba desde sanbox

--- src/components/TodoCounter.jsx
+++ src/components/TodoCounter.jsx
@@ -1,8 +1,10 @@
-import React  from "react";
+import React from "react";
 
-export function TodoCounter ({total , completed}){
-    let countTodo =  completed.length;
-    return (
-        <h3 className="display-6">Has Complete  {countTodo} of {total} task</h3>
-    )
+export function TodoCounter({ total, completed }) {
+  let countTodo = completed.length;
+  return (
+    <h3 className="display-6">
+      Has Completee {countTodo} of {total} task
+    </h3>
+  );
 }
