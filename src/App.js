7185723353341d547367405126744f25157ee4f5
Juan Camilo Penagos <109445222+Misterdev19@users.noreply.github.com>
2022-08-16 19:41:04 -0500

    custumer hook and local Strorange

--- src/App.js
+++ src/App.js
@@ -1,60 +1,77 @@
 import React from "react";
 import { useState } from "react";
-import { TodoCounter } from './components/TodoCounter';
-import { TodoItem } from './components/TodoItem';
-import { TodoList } from './components/TodoList';
-import { TodoSearch } from './components/TodoSearch';
-import { CreatTodoButton } from './components/CreatTodoButton';
-import urlImg from './img/logo.jpg';
-import { Table } from 'reactstrap';
-import './asset/css/styles.css';
+import { TodoCounter } from "./components/TodoCounter";
+import { TodoItem } from "./components/TodoItem";
+import { TodoList } from "./components/TodoList";
+import { TodoSearch } from "./components/TodoSearch";
+import { CreatTodoButton } from "./components/CreatTodoButton";
+import urlImg from "./img/logo.jpg";
+import { Table } from "reactstrap";
+import "./asset/css/styles.css";
 
-const defaulTodos = [
-  { text: 'Botar la Basura', completed: false },
-  { text: 'Lavar los platos', completed: false },
-  { text: 'Comprar comdida', completed: false },
-  { text: 'Estudiar Reactjs', completed: false }
-]
+// const defaulTodos = [
+//   { text: "Botar la Basura", completed: false },
+//   { text: "Lavar los platos", completed: false },
+//   { text: "Comprar comdida", completed: false },
+//   { text: "Estudiar Reactjs", completed: false }
+// ];
+function useLocalStorange(itemName, initialValue) {
+  const localStorangItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if (!localStorangItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorangItem);
+  }
+
+  const [item, setItem] = useState(parsedItem);
+  //connection between localStorange and the status
+  const saveItem = (newTItem) => {
+    const stringiTItem = JSON.stringify(newTItem);
+    localStorage.setItem(itemName, stringiTItem);
+    setItem(newTItem);
+  };
+  return [item, saveItem];
+}
 
 function App() {
+  const [todos, saveTodo] = useLocalStorange("TODOS_V1", []);
+  const [searchValue, setSearchValue] = useState("");
 
-  const [todos, setTodo] = useState(defaulTodos);
-  const [searchValue, setSearchValue] = useState('');
-
-  const completedTodos = todos.filter(todo => !!todo.completed);
+  const completedTodos = todos.filter((todo) => !!todo.completed);
 
   let todoFilter = todos;
 
   if (searchValue.length > 2) {
-    todoFilter = todos.filter(todo => todo.text.toLocaleLowerCase().indexOf(searchValue.toLowerCase()) > -1);
+    todoFilter = todos.filter(
+      (todo) =>
+        todo.text.toLocaleLowerCase().indexOf(searchValue.toLowerCase()) > -1
+    );
   }
 
   const totalTodos = todos.length;
-
   //Function for  change status of  task
   const completedTask = (text) => {
-    let indexTask = todos.findIndex(todo => todo.text === text);
+    let indexTask = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos[indexTask].completed = true;
-    setTodo(newTodos);
-  }
+    saveTodo(newTodos);
+  };
   //Fuction for delete task
   const deleteTask = (text) => {
-    let indexTask = todos.findIndex(todo => todo.text === text);
+    let indexTask = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(indexTask, 1);
-    setTodo(newTodos);
-  }
-
+    saveTodo(newTodos);
+  };
 
   return (
     <div className="container con">
       <div className="row">
         <div className="col-sm-5">
-          <TodoCounter
-            total={totalTodos}
-            completed={completedTodos}
-          />
+          <TodoCounter total={totalTodos} completed={completedTodos} />
         </div>
         <div className="row">
           <div className="col-sm-4">
@@ -76,39 +93,33 @@ function App() {
             <Table bordered hover>
               <thead>
                 <tr>
-                  <th>
-                    #
-                  </th>
-                  <th>
-                    Name task
-                  </th>
-                  <th>
-                    Status
-                  </th>
-                  <th>
-                    Settings
-                  </th>
+                  <th>#</th>
+                  <th>Name task</th>
+                  <th>Status</th>
+                  <th>Settings</th>
                 </tr>
               </thead>
               <tbody>
                 <TodoList>
-                  {todoFilter.map((todo, index) =>
+                  {todoFilter.map((todo, index) => (
                     <TodoItem
                       key={todo.text}
                       text={todo.text}
                       st={todo.completed}
                       id={index}
-                      onComplete={() => { completedTask(todo.text) }}
+                      onComplete={() => {
+                        completedTask(todo.text);
+                      }}
                       onDelete={() => deleteTask(todo.text)}
-                       />
-                  )}
+                    />
+                  ))}
                 </TodoList>
               </tbody>
             </Table>
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 }
 export default App;
